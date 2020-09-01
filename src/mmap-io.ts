8cc98fc608b254c9380afb6dbe08e6509fb5a2d6
Nick Johnstone <ncwjohnstone@gmail.com>
2020-09-01 13:19:28 +1200

    Fix package.json path for bindings lookup

--- src/mmap-io.ts
+++ src/mmap-io.ts
@@ -1,6 +1,6 @@
 const binary = require('node-pre-gyp');
 const path = require('path');
-const binding_path = binary.find(path.resolve(path.join(__dirname,'../package.json')));
+const binding_path = binary.find(path.resolve(path.join(__dirname,'../../package.json')));
 const mmap_lib_raw_ = require(binding_path);
 
 type FileDescriptor = number
