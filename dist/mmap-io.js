dd414dbe45d2b4aa4a3a04c62fe606643d8fe163
Nick Johnstone <ncwjohnstone@gmail.com>
2021-08-27 17:22:28 +1200

    Require correct node-pre-gyp module, run tests on travis

--- dist/mmap-io.js
+++ dist/mmap-io.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const binary = require('node-pre-gyp');
+const binary = require('@mapbox/node-pre-gyp');
 const path = require('path');
 const binding_path = binary.find(path.resolve(path.join(__dirname, '../package.json')));
 const mmap_lib_raw_ = require(binding_path);
